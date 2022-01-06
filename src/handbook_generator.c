efe596578cdce4d2d3a4af219c97cde60522cd81
billy <tyrrell.simon@gmail.com>
2022-01-06 19:31:16 +0000

    escaped latex special characters in text blocks

--- src/handbook_generator.c
+++ src/handbook_generator.c
@@ -29,7 +29,7 @@
 #include "programme.h"
 #include "handbook_generator.h"
 
-static bool InsertLatexTabularRow (FILE *out_f, const char * const key_s, const char * const value_s);
+static bool InsertLatexTabularRow (FILE *out_f, const char * const key_s, const char * const value_s, ByteBuffer *buffer_p);
 static bool InsertPersonAsLatexTabularRow (FILE *out_f, const char * const key_s, const Person * const person_p);
 static bool InsertLatexTabularRowAsHyperlink (FILE *out_f, const char * const key_s, const char * const value_s);
 static bool InsertLatexTabularRowAsUint (FILE *out_f, const char * const key_s, const uint32 * const value_p);
@@ -57,89 +57,99 @@ OperationStatus GenerateStudyAsPDF (const Study *study_p, FieldTrialServiceData 
 
 							if (study_tex_f)
 								{
-									const FieldTrial *trial_p = study_p -> st_parent_p;
-									const Programme *programme_p = trial_p ? trial_p -> ft_parent_p : NULL;
+									ByteBuffer *buffer_p = AllocateByteBuffer (4096);
 
-									/* start the doc and use a sans serif font */
-									fputs ("\\documentclass[a4paper,12pt]{article}\n", study_tex_f);
-
-									fputs ("\\usepackage{tabularx}\n", study_tex_f);
-									fputs ("\\usepackage{hyperref}\n", study_tex_f);
-									fputs ("\\usepackage[margin=0.6in]{geometry}\n", study_tex_f);
-
-									fputs ("\\urlstyle{same}\n\\hypersetup{\n\tcolorlinks=true,\n\tlinkcolor=blue,\n\tfilecolor=blue,\n\turlcolor=blue,\n", study_tex_f);
-
-									fprintf (study_tex_f, "\tpdftitle={%s}\n}\n", study_p -> st_name_s);
-
-
-									fprintf (study_tex_f, "\\title {%s}\n", study_p -> st_name_s);
-
-									fputs ("\\begin{document}\n\\sffamily\n", study_tex_f);
-
-									fputs ("\\maketitle\n", study_tex_f);
-
-									if (programme_p)
+									if (buffer_p)
 										{
-											fputs ("\\section* {Programme}\n", study_tex_f);
+
+											const FieldTrial *trial_p = study_p -> st_parent_p;
+											const Programme *programme_p = trial_p ? trial_p -> ft_parent_p : NULL;
+
+											/* start the doc and use a sans serif font */
+											fputs ("\\documentclass[a4paper,12pt]{article}\n", study_tex_f);
+
+											fputs ("\\usepackage{tabularx}\n", study_tex_f);
+											fputs ("\\usepackage{hyperref}\n", study_tex_f);
+											fputs ("\\usepackage[margin=0.6in]{geometry}\n", study_tex_f);
+
+											fputs ("\\urlstyle{same}\n\\hypersetup{\n\tcolorlinks=true,\n\tlinkcolor=blue,\n\tfilecolor=blue,\n\turlcolor=blue,\n", study_tex_f);
+
+											fprintf (study_tex_f, "\tpdftitle={%s}\n}\n", study_p -> st_name_s);
+
+
+											fprintf (study_tex_f, "\\title {%s}\n", study_p -> st_name_s);
+
+											fputs ("\\begin{document}\n\\sffamily\n", study_tex_f);
+
+											fputs ("\\maketitle\n", study_tex_f);
+
+											if (programme_p)
+												{
+													fputs ("\\section* {Programme}\n", study_tex_f);
+
+													fputs ("\\begin{tabularx}{1\\textwidth}{l X}\n", study_tex_f);
+
+													InsertLatexTabularRow (study_tex_f, "Name", programme_p -> pr_name_s, buffer_p);
+													InsertLatexTabularRow (study_tex_f, "Objective", programme_p -> pr_objective_s, buffer_p);
+
+													InsertPersonAsLatexTabularRow (study_tex_f, "Principal Investigator", programme_p -> pr_pi_p);
+
+													InsertLatexTabularRowAsHyperlink (study_tex_f, "Web Address", programme_p -> pr_documentation_url_s);
+
+											    fputs ("\\end{tabularx}\n", study_tex_f);
+												}		/* if (trial_p) */
+
+
+											if (trial_p)
+												{
+													fputs ("\\section* {Field Trial}\n", study_tex_f);
+
+													fputs ("\\begin{tabularx}{1\\textwidth}{l X}\n", study_tex_f);
+
+											    InsertLatexTabularRow (study_tex_f, "Name", trial_p -> ft_name_s, buffer_p);
+													InsertLatexTabularRow (study_tex_f, "Team", trial_p -> ft_team_s, buffer_p);
+
+											    fputs ("\\end{tabularx}\n", study_tex_f);
+												}		/* if (trial_p) */
+
+
+											fputs ("\\section* {Study}\n", study_tex_f);
 
 											fputs ("\\begin{tabularx}{1\\textwidth}{l X}\n", study_tex_f);
 
-											InsertLatexTabularRow (study_tex_f, "Name", programme_p -> pr_name_s);
-											InsertLatexTabularRow (study_tex_f, "Objective", programme_p -> pr_objective_s);
+											InsertLatexTabularRow (study_tex_f, "Name", study_p -> st_name_s, buffer_p);
+											InsertLatexTabularRow (study_tex_f, "Description", study_p -> st_description_s, buffer_p);
 
-											InsertPersonAsLatexTabularRow (study_tex_f, "Principal Investigator", programme_p -> pr_pi_p);
+											InsertLatexTabularRowAsUint (study_tex_f, "Sowing Year", study_p -> st_predicted_sowing_year_p);
+											InsertLatexTabularRowAsUint (study_tex_f, "Harvest Year", study_p -> st_predicted_harvest_year_p);
 
-											InsertLatexTabularRowAsHyperlink (study_tex_f, "Web Address", programme_p -> pr_documentation_url_s);
+
+											InsertLatexTabularRowAsHyperlink (study_tex_f, "Web Address", study_p -> st_data_url_s);
+
+											InsertLatexTabularRow (study_tex_f, "Design", study_p -> st_design_s, buffer_p);
+											InsertLatexTabularRow (study_tex_f, "Growing Conditions", study_p -> st_growing_conditions_s, buffer_p);
+
+											InsertPersonAsLatexTabularRow (study_tex_f, "Contact", study_p -> st_contact_p);
+											InsertPersonAsLatexTabularRow (study_tex_f, "Curator", study_p -> st_curator_p);
+
+											InsertLatexTabularRow (study_tex_f, "Phenotype Gathering Notes", study_p -> st_phenotype_gathering_notes_s, buffer_p);
+											InsertLatexTabularRow (study_tex_f, "Physical Samples Collected", study_p -> st_physical_samples_collected_s, buffer_p);
+
+											InsertLatexTabularRow (study_tex_f, "Changes to Plan", study_p -> st_plan_changes_s, buffer_p);
+											InsertLatexTabularRow (study_tex_f, "Data not stored in Grassroots", study_p -> st_data_not_included_s, buffer_p);
+
+
+											InsertLatexTabularRow (study_tex_f, "Weather", study_p -> st_weather_link_s, buffer_p);
+
 
 									    fputs ("\\end{tabularx}\n", study_tex_f);
-										}		/* if (trial_p) */
+
+											fputs ("\\end{document}\n", study_tex_f);
+
+											FreeByteBuffer (buffer_p);
+										}		/* if (buffer_p); */
 
 
-									if (trial_p)
-										{
-											fputs ("\\section* {Field Trial}\n", study_tex_f);
-
-											fputs ("\\begin{tabularx}{1\\textwidth}{l X}\n", study_tex_f);
-
-									    InsertLatexTabularRow (study_tex_f, "Name", trial_p -> ft_name_s);
-											InsertLatexTabularRow (study_tex_f, "Team", trial_p -> ft_team_s);
-
-									    fputs ("\\end{tabularx}\n", study_tex_f);
-										}		/* if (trial_p) */
-
-
-									fputs ("\\section* {Study}\n", study_tex_f);
-
-									fputs ("\\begin{tabularx}{1\\textwidth}{l X}\n", study_tex_f);
-
-									InsertLatexTabularRow (study_tex_f, "Name", study_p -> st_name_s);
-									InsertLatexTabularRow (study_tex_f, "Description", study_p -> st_description_s);
-
-									InsertLatexTabularRowAsUint (study_tex_f, "Sowing Year", study_p -> st_predicted_sowing_year_p);
-									InsertLatexTabularRowAsUint (study_tex_f, "Harvest Year", study_p -> st_predicted_harvest_year_p);
-
-
-									InsertLatexTabularRowAsHyperlink (study_tex_f, "Web Address", study_p -> st_data_url_s);
-
-									InsertLatexTabularRow (study_tex_f, "Design", study_p -> st_design_s);
-									InsertLatexTabularRow (study_tex_f, "Growing Conditions", study_p -> st_growing_conditions_s);
-
-									InsertPersonAsLatexTabularRow (study_tex_f, "Contact", study_p -> st_contact_p);
-									InsertPersonAsLatexTabularRow (study_tex_f, "Curator", study_p -> st_curator_p);
-
-									InsertLatexTabularRow (study_tex_f, "Phenotype Gathering Notes", study_p -> st_phenotype_gathering_notes_s);
-									InsertLatexTabularRow (study_tex_f, "Physical Samples Collected", study_p -> st_physical_samples_collected_s);
-
-									InsertLatexTabularRow (study_tex_f, "Changes to Plan", study_p -> st_plan_changes_s);
-									InsertLatexTabularRow (study_tex_f, "Data not stored in Grassroots", study_p -> st_data_not_included_s);
-
-
-									InsertLatexTabularRow (study_tex_f, "Weather", study_p -> st_weather_link_s);
-
-
-							    fputs ("\\end{tabularx}\n", study_tex_f);
-
-									fputs ("\\end{document}\n", study_tex_f);
 									fclose (study_tex_f);
 								}		/* if (study_tex_f) */
 
@@ -170,13 +180,22 @@ static bool InsertLatexTabularRowAsUint (FILE *out_f, const char * const key_s, 
 }
 
 
-static bool InsertLatexTabularRow (FILE *out_f, const char * const key_s, const char * const value_s)
+static bool InsertLatexTabularRow (FILE *out_f, const char * const key_s, const char * const value_s, ByteBuffer *buffer_p)
 {
 	bool success_flag = true;
 
   if (value_s)
   	{
-			success_flag = (fprintf (out_f, "\\textbf{%s}: & %s \\\\\n", key_s, value_s) > 0);
+  		if (EscapeLatexCharacters (buffer_p, value_s))
+  			{
+  				success_flag = (fprintf (out_f, "\\textbf{%s}: & %s \\\\\n", key_s, GetByteBufferData (buffer_p)) > 0);
+  			}
+  		else
+  			{
+  				success_flag = false;
+  			}
+
+  		ResetByteBuffer (buffer_p);
   	}
 
   return success_flag;
@@ -231,12 +250,27 @@ static bool EscapeLatexCharacters (ByteBuffer *buffer_p, const char *input_s)
 		{
 			if (strchr (chars_to_escape_p, *input_s))
 				{
+					const char esc = '\\';
+
+					if (!AppendToByteBuffer (buffer_p, &esc, 1))
+						{
+							success_flag = false;
+						}
+
+				}
+
+			if (success_flag)
+				{
 					if (AppendToByteBuffer (buffer_p, input_s, 1))
 						{
 							++ input_s;
 
 							loop_flag = (*input_s != '\0');
 						}
+				}
+			else
+				{
+					success_flag = false;
 				}
 		}
 
